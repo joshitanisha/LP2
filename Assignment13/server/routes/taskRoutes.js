0d20e2cb85e1051659e2c46126a3f26275977706
joshitanisha <143516784+joshitanisha@users.noreply.github.com>
2026-05-15 22:01:37 +0530

    Implement task routes for CRUD operations

---
+++ Assignment13/server/routes/taskRoutes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const router = express.Router();
+
+const Task = require('../models/Task');
+
+router.get('/', async (req, res) => {
+
+  const tasks = await Task.find();
+
+  res.json(tasks);
+
+});
+
+router.post('/', async (req, res) => {
+
+  const task = new Task({
+    title: req.body.title
+  });
+
+  await task.save();
+
+  res.json(task);
+
+});
+
+router.delete('/:id', async (req, res) => {
+
+  await Task.findByIdAndDelete(req.params.id);
+
+  res.json({
+    message: 'Task Deleted'
+  });
+
+});
+
+module.exports = router;
